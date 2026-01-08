540410fd38a9c825d815287fa7b77bb4aac88b5a
AbelMuro <abelmuro93@gmail.com>
2026-01-07 16:01:23 -0800

    commit

---
+++ src/Routes/GET/Authorization.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/authorization', (req, res) => {
+    try{
+        const accessToken = req.cookies.accessToken;
+
+        if(accessToken)
+            res.status(200).send('User is logged in');
+        else
+            res.status(401).send('User is not logged in');
+    }
+    catch(error){
+        const message = error.message;
+        console.log(message);
+        res.status(500).send(message);
+    }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const RegisterAccount = require('./Routes/POST/RegisterAccount.js');
 const Login = require('./Routes/PUT/Login.js');
 const ForgotPassword = require('./Routes/PUT/ForgotPassword.js');
 const ResetPassword = require('./Routes/PUT/ResetPassword.js');
+const Authorization = require('./Routes/GET/Authorization.js');
 const port = 4000;
 
 
@@ -23,6 +24,7 @@ app.use(RegisterAccount);
 app.use(Login);
 app.use(ForgotPassword);
 app.use(ResetPassword);
+app.use(Authorization);
 
 app.get('/', (req, res) => {
     res.send('hello world');
